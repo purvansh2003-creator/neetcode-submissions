0de6c9123ea4744deb65cff145515353e1883362
Purvansh <purvansh2003@gmail.com>
2026-08-04 17:54:25 +0530

    Add: duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) 
+    {
+        unordered_set <int> distinct;
+        for(int i =0;i<nums.size();i++)
+        {
+            distinct.insert(nums[i]);
+        }
+        if(nums.size() != distinct.size())
+        return true;
+        else
+        return false;
+    }
+};
